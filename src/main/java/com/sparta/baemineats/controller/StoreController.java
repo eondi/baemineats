017aa327542ef3f9a0ed92e98aeff38073f435bf
kiwimel0n <129644629+kiwimel0n@users.noreply.github.com>
2024-02-27 10:53:52 +0900

    Update StoreController.java

--- src/main/java/com/sparta/baemineats/controller/StoreController.java
+++ src/main/java/com/sparta/baemineats/controller/StoreController.java
@@ -2,7 +2,7 @@ package com.sparta.baemineats.controller;
 
 import com.sparta.baemineats.dto.requestDto.StroeRequest;
 import com.sparta.baemineats.dto.responseDto.ResponseForm;
-import com.sparta.baemineats.dto.responseDto.StroeResponse;
+import com.sparta.baemineats.dto.responseDto.StoreResponse;
 import com.sparta.baemineats.security.UserDetailsImpl;
 import com.sparta.baemineats.service.StoreService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -28,7 +28,7 @@ public class StoreController {
     @Operation(summary = "음식점 등록", description = "음식점을 등록한다")
     // 관리자로 등록시에 user의id가 관리자의 id가 되는 부분
     public ResponseEntity<ResponseForm> createStore(@RequestBody StroeRequest request, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        StroeResponse response = storeService.createStore(request, userDetails.getUser());
+        StoreResponse response = storeService.createStore(request, userDetails.getUser());
 
         return ResponseEntity.ok()
                 .body(ResponseForm.builder()
@@ -41,7 +41,7 @@ public class StoreController {
     @GetMapping("/all")
     @Operation(summary = "음식점 목록 전체 조회", description = "음식점 전체 목록 조회를 한다")
     public ResponseEntity<ResponseForm> getStoreNames(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        List<StroeResponse> response = storeService.getStoreNames(userDetails.getUser());
+        List<StorResponse> response = storeService.getStoreNames(userDetails.getUser());
 
         return ResponseEntity.ok()
                 .body(ResponseForm.builder()
@@ -54,7 +54,7 @@ public class StoreController {
     @GetMapping("/{storeId}")
     @Operation(summary = "음식점 상세 조회", description = "특정 음식점 상세 조회를 한다")
     public ResponseEntity<ResponseForm> getStore(@PathVariable Long storeId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        StroeResponse response = storeService.getStore(storeId, userDetails.getUser());
+        StoreResponse response = storeService.getStore(storeId, userDetails.getUser());
 
         return ResponseEntity.ok()
                 .body(ResponseForm.builder()
@@ -67,7 +67,7 @@ public class StoreController {
     @PutMapping("{storeId}")
     @Operation(summary = "음식점 수정", description = "특정 음식점을 수정 한다")
     public ResponseEntity<ResponseForm> updateStore(@PathVariable Long storeId, @RequestBody StroeRequest request, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        StroeResponse response = storeService.updateStore(storeId, request, userDetails.getUser());
+        StoreResponse response = storeService.updateStore(storeId, request, userDetails.getUser());
 
         return ResponseEntity.ok()
                 .body(ResponseForm.builder()
