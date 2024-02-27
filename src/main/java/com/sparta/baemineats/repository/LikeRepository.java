c70f382c6ea29cc7a00f3bac317e0213fff2b12c
ejchae <ejchae@kissco.co.jp>
2024-02-27 17:59:31 +0900

    Feat : like관련기능 Refactor: store에 있던 기능 위치 변경
    조회, 생성, 삭제

--- src/main/java/com/sparta/baemineats/repository/LikeRepository.java
+++ src/main/java/com/sparta/baemineats/repository/LikeRepository.java
@@ -3,10 +3,16 @@ package com.sparta.baemineats.repository;
 import com.sparta.baemineats.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like,Long> {
-    Optional<Object> findByStore_StoreId(Long storeId);
+    List<Like> findByStore_StoreId(Long storeId);
 
-    Optional<Object> findByUser_UserId(Long userId);
+    List<Like> findByUser_UserId(Long userId);
+
+    List<Like> findALLByStore_StoreId(Long storeId);
+
+    List<Like> findALLByUser_UserId(Long storeId);
+
 }
