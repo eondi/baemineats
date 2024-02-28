3d613ac4aca25fa0dfaa6bd68ecc578b474bfce1
ejchae <ejchae@kissco.co.jp>
2024-02-28 10:08:19 +0900

    Test : 테스트코드 관련 폴더,환경 추가

---
+++ src/test/java/com/sparta/baemineats/controller/StoreControllerTest.java
@@ -0,0 +1,7 @@
+package com.sparta.baemineats.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreControllerTest {
+
+}
---
+++ src/test/java/com/sparta/baemineats/dto/requestDto/StoreRequestTest.java
@@ -0,0 +1,7 @@
+package com.sparta.baemineats.dto.requestDto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreRequestTest {
+
+}
