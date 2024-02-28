02f4aa0a77380cf1f176e777e57c160becd4a0fa
Ssuminn <wert159311@gmail.com>
2024-02-28 22:36:35 +0900

    Feat : CartRequest 추가 @NoArgsConstructor 추가

--- src/main/java/com/sparta/baemineats/dto/requestDto/CartRequest.java
+++ src/main/java/com/sparta/baemineats/dto/requestDto/CartRequest.java
@@ -1,12 +1,22 @@
 package com.sparta.baemineats.dto.requestDto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class CartRequest {
     private Long storeId;
     private Long userId;
     private Long menuId;
     private Long quantity;
     private int totalPrice;
+
+    public CartRequest(Long storeId, Long userId, Long menuId, Long quantity, int totalPrice) {
+        this.storeId = storeId;
+        this.userId = userId;
+        this.menuId =  menuId;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
 }
