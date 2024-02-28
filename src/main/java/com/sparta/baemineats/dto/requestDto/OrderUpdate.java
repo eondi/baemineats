7f0dd5b266618ec363c0cedbcf68f3a371500d3d
Ssuminn <wert159311@gmail.com>
2024-02-29 01:21:28 +0900

    Feat : 어노테이션 추가

--- src/main/java/com/sparta/baemineats/dto/requestDto/OrderUpdate.java
+++ src/main/java/com/sparta/baemineats/dto/requestDto/OrderUpdate.java
@@ -1,12 +1,13 @@
 package com.sparta.baemineats.dto.requestDto;
 
 import com.sparta.baemineats.entity.Order.OrderStateEnum;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@Getter
 public class OrderUpdate {
     private OrderStateEnum orderState;
-
-    public OrderUpdate() {
-    }
 
     public OrderUpdate(OrderStateEnum orderState) {
         this.orderState = orderState;
